c99bac6351ab8040f0f70ce598c00be65dbe6895
JulesFert <jfert.lyon@gmail.com>
2023-03-17 22:54:14 +0100

    Porte Folio Jules début

--- head.php
+++ head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.cv.css">
     <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=balsamiq-sans:400|open-sans:400" rel="stylesheet" />
     <title><?php if (isset($title)) { echo $title; } else { echo "Mon site";} ?></title>
